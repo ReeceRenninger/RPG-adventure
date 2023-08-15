1429a50be49688f8fd8a67d5ce238a203e9d4ec0
ReeceRenninger <renningerreece@gmail.com>
2023-08-15 15:50:06 -0700

    trying to figure out how to allow users to get a choice of classes to pick at the start of the game with different starting health

--- character.py
+++ character.py
@@ -9,6 +9,37 @@ class Character:
         self.inventory.append(item)
 
         # look into other methods for possibly item breaking, expand inventory to be larger, etc.
+class Barbarian(Character):
+    def __init__(self, name):
+        super().__init__(name, health = 150)
+
+class Paladin(Character):
+    def __init__(self, name):
+        super().__init__(name, health = 120)
+
+class Ranger(Character):
+    def __init__(self, name):
+        super().__init__(name, health = 100)
+
+def choose_character_class():
+    print("Choose your class: ")
+    print("1. Barbarian")
+    print("2. Paladin")
+    print("3. Ranger")
+
+    choice = input("Enter the number of your choice:")
+    name = input("Enter your character's name: ")
+
+    if choice == "1":
+        return Barbarian(name)
+    elif choice == "2":
+        return Paladin(name)
+    elif choice == "3":
+        return Ranger(name)
+    else:
+        print("Invalid choice. Please try again.")
+        return choose_character_class()
+
 
 # base item creator
 class Items:
@@ -17,4 +48,10 @@ class Items:
        
 
 
+def main():
+    player = choose_character_class()
+    print(f"Welcome, {player.name}!")
+    print(f"You've chosen the {type(player).__name__} class.) class with {player.health} health.))")
 
+if __name__ == "__main__":
+    main()
